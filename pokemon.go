7fa118d6678448bf90540a27ca6c0303c37c2263
Julien CANON <julien.canon@gfi.fr>
2020-08-05 15:43:40 +0000

    Implement GetPokemon

--- pokemon.go
+++ pokemon.go
@@ -53,3 +53,25 @@ func (c *Client) CreatePokemon(pokemon_in Pokemon) (*Pokemon, error) {
 
 	return &pokemon_out, nil
 }
+
+// GetPokemon - Returns a specifc pokemon
+func (c *Client) GetPokemon(pokemonID string) (*Pokemon, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/pokemons/%s", c.HostURL, pokemonID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	pokemon := Pokemon{}
+	err = json.Unmarshal(body, &pokemon)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pokemon, nil
+}
+
